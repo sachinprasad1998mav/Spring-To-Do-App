395fc71a0b1ebdbd01b47681b02827a98d8ce5ff
Sachin <sachinprasad1998mav@gmail.com>
2024-04-30 12:11:14 -0500

    Basic setup done.

---
+++ src/main/java/com/SpringBoot/Sachin/myToDoApp/MyToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Sachin.myToDoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyToDoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot/Sachin/myToDoApp/MyToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Sachin.myToDoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
